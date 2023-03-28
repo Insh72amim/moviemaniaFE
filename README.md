15957fe142380642f99017086d1c14d93d00c9f0
Insh72amim <42885018+Insh72amim@users.noreply.github.com>
2023-03-28 23:49:33 +0000

    Build: (d3f0f08) Merge pull request #3 from Insh72amim/development
    updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Getting Started with Create React App
-
-Updated Read me file
-
-This is Front End part of movie mania application!!!!
--- src/api/axiosConfig.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://moviemania-production.up.railway.app",
-    headers: { "ngrok-skip-browser-warning": "true" },
-});
